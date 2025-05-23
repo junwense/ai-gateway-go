ea5591cf66232621867b6b0f14d21585c9bc5578
junwense <970597653@qq.com>
2025-05-23 18:05:54 +0800

    feat: 1.test

--- .script/mysql/init.sql
+++ .script/mysql/init.sql
@@ -1,1 +1,4 @@
-create database if not exists ai_gateway;
+CREATE DATABASE IF NOT EXISTS ai_gateway;
+-- 授权root用户从任意IP访问（根据实际需求缩小范围）
+GRANT ALL PRIVILEGES ON ai_gateway.* TO 'root'@'%' IDENTIFIED BY 'root';
+FLUSH PRIVILEGES;
