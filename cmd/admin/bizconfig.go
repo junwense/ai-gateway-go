9f12439799cc751a957dd7f6818361d2dfd8eacd
junwense <970597653@qq.com>
2025-05-26 10:33:05 +0800

    fix: 1.修复扫描问题

--- cmd/admin/bizconfig.go
+++ cmd/admin/bizconfig.go
@@ -1,3 +1,17 @@
+// Copyright 2021 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -18,7 +32,10 @@ func main() {
 	server := gin.Default()
 	bizconfig := initBizConfig(db)
 	bizconfig.RegisterRoutes(server)
-	server.Run(":8080")
+	err := server.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
 
 // initDB 初始化数据库并自动建表
@@ -36,8 +53,8 @@ func initDB() *gorm.DB {
 
 // InitBizConfigService 初始化 BizConfigService 实例
 func initBizConfig(db *gorm.DB) *web.BizConfigHandler {
-	dao := dao.NewBizConfigDAO(db)
-	repo := repository.NewBizConfigRepository(dao)
+	dao1 := dao.NewBizConfigDAO(db)
+	repo := repository.NewBizConfigRepository(dao1)
 	svc := service.NewBizConfigService(repo)
 	server := web.NewBizConfigHandler(svc)
 	return server
